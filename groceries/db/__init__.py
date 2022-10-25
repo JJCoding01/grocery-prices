06e9e3ab836676ba1b817f140b49caa98cc238c7
Joseph Contreras <26684136+JosephJContreras@users.noreply.github.com>
2022-10-24 21:21:27 -0500

    Rewrite db.__init__

--- groceries/db/__init__.py
+++ groceries/db/__init__.py
@@ -1,6 +1,28 @@
 # pylint: disable=F
 # flake8: noqa
 
-from .base import Base
+from pathlib import Path
 
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import Session
+
+Base = declarative_base()
+
+
+# must import models after Base is defined to avoid circular import error
 from . import models  # isort:skip
+from . import populate  # isort:skip
+
+# path to project root
+PATH = Path(__file__).parent.parent.parent.absolute()
+
+DATABASE_URI = f"sqlite:///{PATH / 'test_groceries.db'}"
+
+engine = create_engine(DATABASE_URI)
+session = Session(bind=engine)
+
+
+def recreate_all():
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine, checkfirst=False)
