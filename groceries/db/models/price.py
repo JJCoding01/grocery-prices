b929ba6143e61ea408fb6f098b2fc69c82a15452
Joseph Contreras <26684136+JosephJContreras@users.noreply.github.com>
2022-11-05 18:31:23 -0500

    Simplify models.Price by removing columns
    Remove the following, unused columns:
       * Date
       * Check price
       * note
    Add preference type column

--- groceries/db/models/price.py
+++ groceries/db/models/price.py
@@ -22,26 +22,21 @@ class Price(Base):
     price_id = Column("PriceID", Integer, primary_key=True, autoincrement=True)
     store_id = Column("StoreID", Integer, ForeignKey("Stores.StoreID"), nullable=False)
     item_id = Column("ItemID", Integer, ForeignKey("Items.ItemID"), nullable=False)
+    preference_type_id = Column(
+        "PreferenceTypeID", Integer, ForeignKey("PreferenceTypes.TypeID"), nullable=True
+    )
 
-    price = Column("Price", Float, CheckConstraint('"Price" > 0'), nullable=False)
-    date = Column("Date", DateTime, nullable=False, default=datetime.today)
-    check_price = Column("Check", Boolean, nullable=False, default=False)
-    note = Column("Notes", String)
+    price = Column("Price", Float, CheckConstraint('"Price" > 0'), nullable=True)
 
     store = relationship("Store")
-    # item = relationship("Item")
+    preference = relationship("PreferenceType")
 
     item = relationship("Item", back_populates="prices")
 
     __table_args__ = (UniqueConstraint("StoreID", "ItemID"),)
 
-    def __init__(self, store, item, price, date=None, check_price=False, note=None):
+    def __init__(self, store, item, price, preference=None):
         self.store = store
         self.item = item
         self.price = price
-        if date is None:
-            self.date = datetime.today()
-        else:
-            self.date = date
-        self.check_price = check_price
-        self.note = note
+        self.preference = preference
