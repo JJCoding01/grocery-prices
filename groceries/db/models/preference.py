95cc894fb4fc448c0582bf8a060743f5fe746b65
Joseph Contreras <26684136+JosephJContreras@users.noreply.github.com>
2022-11-05 18:28:04 -0500

    Update models.PreferenceType to remove note column

--- groceries/db/models/preference.py
+++ groceries/db/models/preference.py
@@ -38,14 +38,10 @@ class PreferenceType(Base):
     type_id = Column("TypeID", Integer, primary_key=True, autoincrement=True)
     type_ = Column("Type", String, nullable=False, unique=True)
     short = Column("ShortType", String, nullable=True)
-    note = Column("Notes", String)
 
-    def __init__(self, type_, short=None, note=None):
+    def __init__(self, type_, short=None):
         self.type_ = type_
         self.short = short
-        self.note = note
 
     def __repr__(self):
-        return (
-            f"{self.__class__.__name__}('{self.type_}', '{self.short}', '{self.note}'"
-        )
+        return f"{self.__class__.__name__}('{self.type_}', '{self.short}'"
