3781d578a0b9e91dd2d5777fc4cb10abb868642b
Joseph Contreras <26684136+JosephJContreras@users.noreply.github.com>
2022-11-05 18:27:01 -0500

    Update models.Item to column with preferred store

--- groceries/db/models/item.py
+++ groceries/db/models/item.py
@@ -12,20 +12,24 @@ class Item(Base):
     category_id = Column(
         "CategoryID", Integer, ForeignKey("Categories.CategoryID"), nullable=True
     )
+    preferred_store_id = Column(
+        "PreferredStoreID", Integer, ForeignKey("Stores.StoreID"), nullable=True
+    )
 
     description = Column("Description", String, nullable=False, unique=True)
     active = Column("Active", Boolean, nullable=False, default=True)
 
     unit = relationship("Unit")
+    preferred_store = relationship("Store")
     category = relationship("Category", back_populates="items")
 
     prices = relationship("Price", back_populates="item")
 
-    def __init__(self, description, unit, category, note=None):
+    def __init__(self, description, unit, category, preferred_store=None):
         self.description = description
         self.unit = unit
         self.category = category
-        self.note = note
+        self.preferred_store = preferred_store
 
     def __repr__(self):
         return f"{self.__class__.__name__}({self.description}, {self.unit}, {self.category})"
