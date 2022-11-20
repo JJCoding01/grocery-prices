61641f9366018e251a6bc2720c58359974087e9f
Joseph Contreras <26684136+JosephJContreras@users.noreply.github.com>
2022-11-20 14:09:51 -0600

    Simplify db schema by removing tables replaced by columns
    * Remove Preference table
    * Delete availability tracking

--- groceries/db/models/__init__.py
+++ groceries/db/models/__init__.py
@@ -1,10 +1,9 @@
 # pylint: disable=F
 # flake8: noqa
 
-from .availability import Availability
 from .category import Category
 from .item import Item
-from .preference import Preference, PreferenceType
+from .preference import PreferenceType
 from .price import Price
 from .store import Store
 from .unit import Unit
--- groceries/db/models/preference.py
+++ groceries/db/models/preference.py
@@ -4,34 +4,6 @@ from sqlalchemy.orm import relationship
 from .. import Base
 
 
-class Preference(Base):
-    __tablename__ = "Preferences"
-
-    preference_id = Column("PriceID", Integer, primary_key=True, autoincrement=True)
-    store_id = Column("StoreID", Integer, ForeignKey("Stores.StoreID"), nullable=False)
-    item_id = Column("ItemID", Integer, ForeignKey("Items.ItemID"), nullable=False)
-    preference_type_id = Column(
-        "PreferenceTypeID",
-        Integer,
-        ForeignKey("PreferenceTypes.TypeID"),
-        nullable=False,
-    )
-
-    note = Column("Notes", String)
-
-    store = relationship("Store")
-    item = relationship("Item")
-    preference = relationship("PreferenceType")
-
-    __table_args__ = (UniqueConstraint("StoreID", "ItemID"),)
-
-    def __init__(self, store, item, preference, note=None):
-        self.store = store
-        self.item = item
-        self.preference = preference
-        self.note = note
-
-
 class PreferenceType(Base):
     __tablename__ = "PreferenceTypes"
 
